7da98d3507fa6e341640fa42dcf4f67c9a248732
Esteban <acevedoesteban999@gmail.com>
2024-11-11 23:55:32 -0500

    FIX: generator without '&'

--- components/eGenerator/eGenerator.c
+++ components/eGenerator/eGenerator.c
@@ -3,24 +3,26 @@
 generator_struct GENERATORS[MAX_GENERATORS];
 
 void write_generator_in_nvs(const char* name, generator_struct*generator){
-    write_struct_in_nvs(name,&generator);
+    write_struct_in_nvs(name,generator);
 }
 
 generator_struct read_generator_in_nvs(const char* name) {
     nvs_handle_t nvs_handle;
     esp_err_t err;
     size_t required_size = sizeof(generator_struct); 
-    generator_struct generator;
+    generator_struct generator = DEFAULT_GENERATOR;
     
     err = open_nvs_handle(&nvs_handle);
     if (err != ESP_OK) {
         ESP_LOGE(TAG_STORE, "Error at open NVS: %s", esp_err_to_name(err));
-        return DEFAULT_GENERATOR;
+        return generator;
     }
 
     err = nvs_get_blob(nvs_handle, name, &generator, &required_size);
-    nvs_close(nvs_handle);
+    if(err != ESP_OK )
+        ESP_LOGE(TAG_STORE, "Error: %s", esp_err_to_name(err));
 
+    nvs_close(nvs_handle);
     return generator;
 }
 
